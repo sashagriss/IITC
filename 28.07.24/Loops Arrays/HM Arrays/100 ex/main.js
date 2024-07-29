391bdeaa482b692bcc519b236b116406a3d63eb1
sashagriss <sasha.meduza24@gmail.com>
2024-07-29 16:08:02 +0300

    arrays

---
+++ 28.07.24/Loops Arrays/HM Arrays/100 ex/main.js
@@ -0,0 +1,38 @@
+// 1.
+let fruits = [];
+// 2.
+let num = [1,2,3,4,5];
+// 3.
+// 4.
+let mixed = [1,true,"sun"]
+// 5.
+let seasons = ["summer","autumn","winter","spring"]
+// 6.
+console.log(num.length);
+// 7.
+let emptyCheck = [];
+if (emptyCheck.length === 0){
+console.log (emptyCheck)
+};
+// 8.
+let dynamic = ["a","b","c","d","e"];
+console.log(dynamic.length);
+// 9.
+dynamic.push("f");
+console.log(dynamic.length);
+// 10.
+dynamic.shift();
+console.log(dynamic.length);
+// 11.
+let colors = ["red","pink","yellow"]
+console.log(colors[0]);
+// 12.
+console.log(seasons[seasons.length -1]);
+// 13.
+midNum = Math.floor(num.length/2)
+console.log(midNum)
+// 14.
+let fruit = ["apple","banana","kiwi"]
+console.log(fruit[6])
+// 15.
+console.log(`THe second color is ${colors[1]}.`);
