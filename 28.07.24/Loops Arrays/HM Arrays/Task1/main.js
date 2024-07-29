46e436139fad7b2eafdadb08cdfda6616c44d180
sashagriss <sasha.meduza24@gmail.com>
2024-07-29 13:28:47 +0300

    arrays

---
+++ 28.07.24/Loops Arrays/HM Arrays/Task1/main.js
@@ -0,0 +1,28 @@
+// Task 1
+
+// 1.
+ let food = ["pinapple","chocolate", "cheese" ,"kiwi", "cream" ];
+// //  2.
+console.log(food[2]);
+// 3.
+food [1]= "milk";
+console.log(food[1]);
+// // 4.
+food.push("apple")
+console.log(food);
+// // 5.
+food.shift();
+console.log(food);
+//  // 6.
+for (let i = 0; i < food.length; i++) {
+   console.log(food[i]);
+};
+// 7.
+console.log(food.indexOf("kiwi"));
+// 8.
+let sum = 0
+let num = [1,2,3,4,5]
+for (i = 0; i < num.length; i++) {
+    sum+= num[i]
+}
+console.log(sum);
