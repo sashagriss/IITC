7a86e6e1372a7fc4207f6f90d7907f6b7bd0aa15
sashagriss <sasha.meduza24@gmail.com>
2024-07-30 22:05:43 +0300

    nested

--- 28.07.24/Loops Arrays/HM Loops/T1/main.js
+++ 28.07.24/Loops Arrays/HM Loops/T1/main.js
@@ -1,9 +1,13 @@
+"use strict"
 // TASK 1
 
-// 1.
-// for (let i = 0; i <=20; i++) {
-//     console.log(i);
-// }
+// 1.*** by Omer
+function countToFrom(from, to) {
+for (let i = from; i <= to; i++) {
+    console.log(i);
+} } 
+// countToFrom(1,20)
+
 
 // 2.
 // let i = 1
@@ -13,7 +17,30 @@
 
 
 // 3.
+// let num = 7
+// do {
+    
+// } while (condition);
 
-
+// 4. 
+function sumFromTo(from, to) {
+    let sum =0
+    for (let i = from ;i <=to; i++) {
+        sum+= i
+    console.log(sum);
+    }
+}
+sumFromTo(3,15)
+// let sum = 0
+// for (let i = 0; i < 100; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+    
   
-  
+// // 5.
+// num= 10
+// while (i>0) { i--
+    
+// }
+// console.log("Blast off");
